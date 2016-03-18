29b4119440245277f9588d15bb2ae720006c7e4c
Tyler Montgomery <tyler@impossibleventures.com>
2016-03-18 16:38:18 -0500

    Update WSGI server

--- proxy/wsgi.py
+++ proxy/wsgi.py
@@ -6,10 +6,11 @@ def application(environ, start_response):
     bucket = os.environ.get('BUCKET')
     no_cache = True if os.environ.get('NO_CACHE') else False
     capacity = int(os.environ.get('CAPACITY', 1000000000))
-    cache_dir = os.environ.get('CACHEDIR')
+    cache_dir = os.environ.get('CACHEDIR', tempfile.mkdtemp(prefix='s3cache-'))
     auth_config = {
         "username": os.environ.get('AUTH_USER', ''),
         "password": os.environ.get('AUTH_PASS', '')
     } if os.environ.get('USE_AUTH') else None
+
     p = CachingS3Proxy(bucket, no_cache, capacity, cache_dir, auth_config)
     return p.proxy_s3_bucket(environ, start_response)
